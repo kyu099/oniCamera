e4d314fd7a16e31092ac652d3601b956e97f0046
kyu099 <ryu_gelus1277@icloud.com>
2021-04-18 20:14:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ const picture = document.getElementById("picture");
 const button = document.getElementById("btn");
 const bigbutton = document.getElementById("big");
 const smallbutton = document.getElementById("small");
+const changebutton = document.getElementById("change");
 
 const ctx = canvas.getContext("2d");
 const ctx2 = canvas2.getContext("2d");
 
+const imglist = ["oni1.png", "oni2.png"];
+
 let img = new Image();
-img.src = "oni1.png";
+let imgnum = 0;
+img.src = imglist[0];
 let size = 200;
 let x = 0;
 let y = 0;
@@ -29,13 +33,6 @@ timer = setInterval(() =>{
     ctx.drawImage(img, x, y, size, size);
 }, 1000/60);
 
-/*
-function draw(){
-}
-*/
-
-
-
 function move(e){
     if(drag){
         x = e.clientX - canvas.getBoundingClientRect().left - size/2;
@@ -49,6 +46,12 @@ button.onclick = () => {
 
 bigbutton.onclick = () => {size += 10};
 smallbutton.onclick = () => {size -= 10};
+changebutton.onclick = () => {
+    if(imgnum < imglist.length - 1) imgnum++;
+    else imgnum = 0;
+    img.src = imglist[imgnum];
+    console.log(imglist.length, imgnum, imglist[imgnum]);
+}
 
 canvas.addEventListener('mousemove', move, false);
 canvas.addEventListener('mousedown', () => {drag = true;}, false);
